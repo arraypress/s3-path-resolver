afba8c104ed61ae6b5230074e036cc94b38c90e6
davidsherlock <43mZGKl$bjD@KfMO%ggj#V5Y5dgrF%ps>
2024-02-11 18:40:36 +0300

    Refactoring work

--- src/Functions.php
+++ src/Functions.php
@@ -33,11 +33,11 @@ if ( ! function_exists( 'parsePath' ) ) {
 	 * Parse the provided path to extract the bucket and object key.
 	 *
 	 * @param string        $path                The S3 path.
-	 * @param string        $defaultBucket       The default bucket to use if none is provided in the path.
-	 * @param array         $allowedExtensions   List of allowed file extensions.
-	 * @param array         $disallowedProtocols List of protocols that are not allowed in S3 paths.
-	 * @param bool          $asArray             Whether to return the result as an array. Default false.
-	 * @param callable|null $errorCallback       Callback function for error handling.
+	 * @param string        $defaultBucket       Optional. The default bucket to use if none is provided in the path.
+	 * @param array         $allowedExtensions   Optional. List of allowed file extensions.
+	 * @param array         $disallowedProtocols Optional. List of protocols that are not allowed in S3 paths.
+	 * @param bool          $asArray             Optional. Whether to return the result as an array. Default false.
+	 * @param callable|null $errorCallback       Optional. Callback function for error handling.
 	 *
 	 * @return array|object|false An associative array or object with 'bucket' and 'object' keys, or false on failure.
 	 * @throws Exception
@@ -62,10 +62,10 @@ if ( ! function_exists( 'isValidPath' ) ) {
 	 * Determines if the provided path is a valid S3 path.
 	 *
 	 * @param string        $path                The path to check.
-	 * @param string        $defaultBucket       The default bucket to use if none is provided in the path.
-	 * @param array         $allowedExtensions   List of allowed file extensions.
-	 * @param callable|null $errorCallback       Callback function for error handling.
-	 * @param array         $disallowedProtocols List of protocols that are not allowed in S3 paths.
+	 * @param string        $defaultBucket       Optional. The default bucket to use if none is provided in the path.
+	 * @param array         $allowedExtensions   Optional. List of allowed file extensions.
+	 * @param array         $disallowedProtocols Optional. List of protocols that are not allowed in S3 paths.
+	 * @param callable|null $errorCallback       Optional. Callback function for error handling.
 	 *
 	 * @return bool True if the path is a valid S3 path, false otherwise.
 	 * @throws Exception
